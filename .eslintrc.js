97d0d9cab7261dbfb62c16182444c1328fbfc09a
Fredrick Yalley <94568827+yalleyfred@users.noreply.github.com>
2024-05-25 22:16:54 +0000

    add: lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,51 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/method-signature-style": ["error", "method"],
+    "arrow-parens": ["off", "always"],
+    "default-case": "error",
+    "default-case-last": "error",
+    "comma-spacing": "error",
+    "padding-line-between-statements": [
+      "error",
+      {
+        "blankLine": "always",
+        "prev": ["const", "let", "var"],
+        "next": "*"
+      },
+      {
+        "blankLine": "any",
+        "prev": ["const", "let", "var"],
+        "next": ["const", "let", "var"]
+      }
+    ],
+    "computed-property-spacing": [
+      "error",
+      "never",
+      {
+        "enforceForClassMembers": true
+      }
+    ],
+    "lines-between-class-members": [
+      "error",
+      "always",
+      {
+        "exceptAfterSingleLine": true
+      }
+    ],
+    "func-name-matching": "error",
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error",
+      {
+        "accessibility": "explicit",
+        "overrides": {
+          "constructors": "no-public",
+          "methods": "explicit",
+          "properties": "explicit",
+          "parameterProperties": "explicit"
+        }
+      }
+    ],
+    "@typescript-eslint/member-ordering": "error"
   },
 };
