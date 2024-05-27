0a6d15afd0a39b4aa43c6ed93978f96f7e879649
Fredrick Yalley <94568827+yalleyfred@users.noreply.github.com>
2024-05-27 13:11:21 +0000

    feat: configure CORS

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,17 @@ async function bootstrap() {
       whitelist: true,
     })
   );
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb', parameterLimit: 999 }));
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+    methods: 'GET, OPTIONS, POST, PUT, PATCH',
+    allowedHeaders: 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With',
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(1000);
 }
 bootstrap();
